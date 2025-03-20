a698fdb88107579783394ee63b991f3923181fc2
HP <sukhveergill18@gmail.com>
2025-03-20 19:22:20 +0530

    my files

---
+++ src/March/ex200325/Lab004.java
@@ -0,0 +1,13 @@
+package March.ex200325;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int table = 13;
+        System.out.printf("%d * %d = %d",table,1,table*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,2,table*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",table ,3,table*3);
+    }
+
+}
